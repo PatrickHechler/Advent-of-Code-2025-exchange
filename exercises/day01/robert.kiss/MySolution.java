50c06abdb9ac0ee1ecf85bf312002d952c382491
Robert Kiss <robert.kiss@telekom.com>
2023-12-05 13:19:17 +0100

    day1 regex

--- exercises/day01/robert.kiss/MySolution.java
+++ exercises/day01/robert.kiss/MySolution.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class MySolution extends MySolutionBase {
 
-    public MySolution(String inputFilename) {
+
+	public MySolution(String inputFilename) {
         super(inputFilename);
     }
 
@@ -21,8 +24,23 @@ public class MySolution extends MySolutionBase {
 			});
 			values.add(10*(cc[0]-48)+cc[1]-48);
 		});
-		System.out.println(values.stream().mapToInt(Integer::intValue).sum());
+		System.out.println("play1: " + values.stream().mapToInt(Integer::intValue).sum());
         return this;
+	}
+
+	private String replacewords(String line) {
+		//System.out.println(line);
+		line = line.replaceAll("one", "one1one");
+		line = line.replaceAll("two", "two2two");
+		line = line.replaceAll("three", "three3three");
+		line = line.replaceAll("four", "four4four");
+		line = line.replaceAll("five", "five5five");
+		line = line.replaceAll("six", "six6six");
+		line = line.replaceAll("seven", "seven7seven");
+		line = line.replaceAll("eight", "eight8eight");
+		line = line.replaceAll("nine", "nine9nine");		
+		//System.out.println(line);
+		return line;
 	}
 
 	private MySolution play2() {
@@ -40,30 +58,47 @@ public class MySolution extends MySolutionBase {
 			});
 			values.add(10*(cc[0]-48)+cc[1]-48);
 		});
-		System.out.println(values.stream().mapToInt(Integer::intValue).sum());
+		System.out.println("play2a: " + values);
+		System.out.println("play2a: " + values.stream().mapToInt(Integer::intValue).sum());
         return this;
 	}
 
-	private String replacewords(String line) {
-		//System.out.println(line);
-		line = line.replaceAll("one", "one1one");
-		line = line.replaceAll("two", "two2two");
-		line = line.replaceAll("three", "three3three");
-		line = line.replaceAll("four", "four4four");
-		line = line.replaceAll("five", "five5five");
-		line = line.replaceAll("six", "six6six");
-		line = line.replaceAll("seven", "seven7seven");
-		line = line.replaceAll("eight", "eight8eight");
-		line = line.replaceAll("nine", "nine9nine");
-		//System.out.println(line);
-		return line;
+	Map<String,Integer> myStrToIntMap = Map.of(
+			"one", 1,
+			"two", 2,
+			"three", 3,
+			"four", 4,
+			"five", 5,
+			"six", 6,
+			"seven", 7,
+			"eight", 8,
+			"nine", 9
+		);
+	private int myStrToInt(String str) {
+		return this.myStrToIntMap.containsKey(str) ? this.myStrToIntMap.get(str) : Integer.parseInt(str);
+
+	}
+	private Pattern myPattern = Pattern.compile("([0-9]|one|two|three|four|five|six|seven|eight|nine).*([0-9]|one|two|three|four|five|six|seven|eight|nine)");
+
+	private MySolution play2b() {
+		List<Integer> values = new ArrayList<Integer>();
+		getInputLinesAsList().forEach(line -> {
+			var mymatcher = myPattern.matcher(line);
+			if (mymatcher.find()) {
+				//System.out.println(mymatcher.group(0) + " -> " + mymatcher.group(1) + "  " + mymatcher.group(2));
+				values.add( 10*myStrToInt(mymatcher.group(1)) + myStrToInt(mymatcher.group(2)));
+			}
+		});
+		System.out.println("play2b: " + values);
+		System.out.println("play2b: " + values.stream().mapToInt(Integer::intValue).sum());
+        return this;
 	}
 
 	public static void main(String args[]) {
 		try {
             new MySolution("sample1.txt").play1();
-            new MySolution("sample2.txt").play2();
-            new MySolution("input.txt").play1().play2();
+            new MySolution("sample2.txt").play2().play2b();
+            new MySolution("input.txt").play1().play2().play2b();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
