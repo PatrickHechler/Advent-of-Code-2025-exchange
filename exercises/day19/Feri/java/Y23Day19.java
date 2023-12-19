9a680a5745db601490709eb0ffa0b2a00e6b45ef
Ferenc Hechler <ferenc.hechler@telekom.de>
2023-12-19 11:05:00 +0100

    feri day 19 part I.

--- exercises/day19/Feri/java/Y23Day19.java
+++ exercises/day19/Feri/java/Y23Day19.java
@@ -6,11 +6,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * see: https://adventofcode.com/2023/day/19
@@ -41,9 +40,14 @@ public class Y23Day19 {
 	 */
 
 	private static final String INPUT_RX_RULE = "^([a-z]+)[{]([0-9a-zAR<>:,]+)[}]$";
+	private static final String INPUT_RX_RULE_COND = "^([xmas])([<>])([0-9]+)[:]([a-zRA]+)$";
+	private static final String INPUT_RX_RULE_FOLLOWUP = "^([a-zRA]+)$";
 	private static final String INPUT_RX_PART = "^[{]([0-9a-z=,]+)[}]$";
+	private static final String INPUT_RX_PART_TEXT = "^x=([0-9]+),m=([0-9]+),a=([0-9]+),s=([0-9]+)$";
 	
-	public static record InputData(String ruleName, String ruleText, String partText) {}
+	public static record InputData(String ruleName, String ruleText, String partText) {
+		public boolean isRule() { return ruleName != null; }
+	}
 	
 	public static class InputProcessor implements Iterable<InputData>, Iterator<InputData> {
 		private Scanner scanner;
@@ -77,9 +81,152 @@ public class Y23Day19 {
 	}
 
 
+	static record Condition(char category, char operator, int value, String acceptName) {
+		public static Condition create(String str) {
+			if (str.matches(INPUT_RX_RULE_COND)) {
+				char category = str.replaceFirst(INPUT_RX_RULE_COND, "$1").charAt(0);
+				char operator = str.replaceFirst(INPUT_RX_RULE_COND, "$2").charAt(0);
+				int value = Integer.parseInt(str.replaceFirst(INPUT_RX_RULE_COND, "$3"));
+				String acceptName = str.replaceFirst(INPUT_RX_RULE_COND, "$4");
+				return new Condition(category, operator, value, acceptName);
+			}
+			else if (str.matches(INPUT_RX_RULE_FOLLOWUP)) {
+				return new Condition('*', '*', 0, str);
+			}
+			else {
+				throw new RuntimeException("invalid condition '"+str+"'");
+			}
+		}
+
+		@Override public String toString() {
+			if (category == '*') {
+				return acceptName; 
+			}
+			return Character.toString(category)+Character.toString(operator)+value+":"+acceptName;
+		}
+
+		public boolean check(Part part) {
+			if (category == '*') {
+				return true;
+			}
+			int compValue = part.get(category);
+			if (operator == '<') {
+				return compValue<value;
+			}
+			return compValue>value;
+		}
+	}
+
+	static class Rule {
+		String name;
+		List<Condition> conditions;
+		public Rule(String name, List<Condition> conditions) {
+			this.name = name;
+			this.conditions = conditions;
+		}
+		public String check(Part part) {
+			for (Condition condition:conditions) {
+				if (condition.check(part)) {
+					return condition.acceptName;
+				}
+			}
+			throw new RuntimeException("RULE does not have a default result "+this);
+		}
+		@Override
+		public String toString() {
+			StringBuilder result = new StringBuilder();
+			result.append(name);
+			String seperator = "{";
+			for (Condition condition:conditions) {
+				result.append(seperator).append(condition.toString());
+				seperator = ",";
+			}
+			result.append("}");
+			return result.toString();
+		}
+	}
+
+	static record Part(int x, int m, int a, int s) {
+		int get(char category) {
+			switch (category) {
+			case 'x': return x;
+			case 'm': return m;
+			case 'a': return a;
+			case 's': return s;
+			default: throw new RuntimeException("invalid category "+category);
+			}
+		}
+		@Override public String toString() {
+			return "{x="+x+",m="+m+",a="+a+",s="+s+"}";
+		}
+		public static Part create(String partText) {
+			int x = Integer.parseInt(partText.replaceFirst(INPUT_RX_PART_TEXT, "$1"));
+			int m = Integer.parseInt(partText.replaceFirst(INPUT_RX_PART_TEXT, "$2"));
+			int a = Integer.parseInt(partText.replaceFirst(INPUT_RX_PART_TEXT, "$3"));
+			int s = Integer.parseInt(partText.replaceFirst(INPUT_RX_PART_TEXT, "$4"));
+			return new Part(x,m,a,s);
+		}
+		public int sum() {
+			return x+m+a+s;
+		}
+	}
+
+
+	public static class World {
+		List<Rule> rules;
+		Map<String, Rule> rulesByName;
+		List<Part> parts;
+		public World() {
+			this.rulesByName = new LinkedHashMap<>();
+			this.rules = new ArrayList<>();
+			this.parts = new ArrayList<>();
+		}
+		public void addRule(String ruleName, String ruleText) {
+			List<Condition> conditions = Stream.of(ruleText.split(",")).map(str -> Condition.create(str)).toList();
+			Rule rule = new Rule(ruleName, conditions);
+			rules.add(rule);
+			rulesByName.put(ruleName, rule);
+		}
+		public void addPart(String partText) {
+			parts.add(Part.create(partText));
+			
+		}
+		@Override
+		public String toString() {
+			return rules.toString() + "\n" + parts.toString();
+		}
+		public List<Part> filterAcceptedParts() {
+			return parts.stream().filter(part->checkAccepted(part)).toList();
+		}
+		private boolean checkAccepted(Part part) {
+			String nextRuleName = "in";
+			while ((!nextRuleName.equals("A"))&&(!nextRuleName.equals("R"))) {
+				Rule rule = rulesByName.get(nextRuleName);
+				nextRuleName = rule.check(part);
+			}
+			System.out.println(part+" -> "+nextRuleName);
+			return nextRuleName.equals("A");
+		}
+	}
+	
+
 	public static void mainPart1(String inputFile) {
+		World world = new World();
 		for (InputData data:new InputProcessor(inputFile)) {
 			System.out.println(data);
+			if (data.isRule()) {
+				world.addRule(data.ruleName, data.ruleText);
+			}
+			else {
+				world.addPart(data.partText);
+			}
+			List<Part> acceptedParts = world.filterAcceptedParts();
+			System.out.println(acceptedParts);
+			int sum = 0;
+			for (Part part:acceptedParts) {
+				sum += part.sum();
+			}
+			System.out.println("ACCEPTEDSUM: "+sum);
 		}
 	}
 
@@ -92,8 +239,8 @@ public class Y23Day19 {
 
 	public static void main(String[] args) throws FileNotFoundException, URISyntaxException {
 		System.out.println("--- PART I ---");
-		mainPart1("exercises/day19/Feri/input-example.txt");
-//		mainPart1("exercises/day19/Feri/input.txt");               
+//		mainPart1("exercises/day19/Feri/input-example.txt");
+		mainPart1("exercises/day19/Feri/input.txt");               
 		System.out.println("---------------");                           
 		System.out.println("--- PART II ---");
 		URL url;
