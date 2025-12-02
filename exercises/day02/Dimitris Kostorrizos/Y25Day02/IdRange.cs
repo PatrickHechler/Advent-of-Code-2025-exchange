94e26a53cc6ea3f6450c30ef39d82a18da0447ae
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2025-12-02 08:03:24 +0200

    Completed the Day 02 exercise

---
+++ exercises/day02/Dimitris Kostorrizos/Y25Day02/IdRange.cs
@@ -0,0 +1,90 @@
+﻿using System.Globalization;
+
+namespace Y25Day02
+{
+    /// <summary>
+    /// Represents a range of ids
+    /// </summary>
+    public sealed class IdRange
+    {
+        /// <summary>
+        /// The dash separator
+        /// </summary>
+        public const char Seperator = '-';
+
+        /// <summary>
+        /// The starting value
+        /// </summary>
+        public long StartingValue { get; }
+
+        /// <summary>
+        /// The ending value
+        /// </summary>
+        public long EndingValue { get; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="IdRange"/>
+        /// </summary>
+        /// <param name="startingValue">The starting value</param>
+        /// <param name="endingValue">The ending value</param>
+        public IdRange(long startingValue, long endingValue) : base()
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(startingValue);
+
+            ArgumentOutOfRangeException.ThrowIfNegative(endingValue);
+
+            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(startingValue, endingValue);
+
+            StartingValue = startingValue;
+
+            EndingValue = endingValue;
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => $"{StartingValue} {Seperator} {EndingValue}";
+
+        /// <summary>
+        /// Creates and returns a <see cref="IdRange"/> from the specified <paramref name="stringRepresentation"/>
+        /// </summary>
+        /// <param name="stringRepresentation">The string representation</param>
+        /// <returns></returns>
+        public static IdRange Create(string stringRepresentation)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(stringRepresentation);
+
+            var parts = stringRepresentation.Split(Seperator, StringSplitOptions.TrimEntries);
+
+            var startingValue = long.Parse(parts[0], CultureInfo.InvariantCulture);
+
+            var endingValue = long.Parse(parts[1], CultureInfo.InvariantCulture);
+
+            return new(startingValue, endingValue);
+        }
+
+        /// <summary>
+        /// Returns the ids contained in the range
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<long> GetRangeIds()
+        {
+            return GenerateSequence(StartingValue, EndingValue);
+        }
+
+        /// <summary>
+        /// Generates a sequence from the <paramref name="start"/> to the <paramref name="end"/> inclusive.
+        /// </summary>
+        /// <param name="start">The starting value</param>
+        /// <param name="end">The ending value</param>
+        /// <returns></returns>
+        private static IEnumerable<long> GenerateSequence(long start, long end)
+        {
+            for (var i = start; i <= end; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
---
+++ exercises/day02/Dimitris Kostorrizos/Y25Day02/Program.cs
@@ -0,0 +1,162 @@
+﻿using System.Globalization;
+
+namespace Y25Day02
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// A  flag indicating whether to use the demo input file or the actual one
+        /// </summary>
+        private static readonly bool _useDemoFile = false;
+
+        private static async Task Main()
+        {
+            await ExecuteFirstHalfAsync();
+
+            await ExecuteSecondHalfAsync();
+        }
+
+        /// <summary>
+        /// Executes the code for the first half of the exercise
+        /// </summary>
+        /// <returns></returns>
+        public static async Task ExecuteFirstHalfAsync()
+        {
+            var fileName = "Input.txt";
+
+            if (_useDemoFile)
+                fileName = "DemoInput.txt";
+
+            var fileContent = await File.ReadAllTextAsync(fileName);
+
+            var idRanges = fileContent.Split(',', StringSplitOptions.TrimEntries);
+
+            var invalidIds = new List<long>();
+
+            foreach (var idRange in idRanges)
+            {
+                var range = IdRange.Create(idRange);
+
+                var rangeIds = range.GetRangeIds();
+
+                invalidIds.AddRange(rangeIds.Where(x => !IsIdValidForFirstHalf(x)));
+            }
+
+            var sum = invalidIds.Sum();
+
+            Console.WriteLine($"The solution is {sum}. Hope you liked it. Press any key to close the console.");
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Executes the code for the second half of the exercise
+        /// </summary>
+        /// <returns></returns>
+        public static async Task ExecuteSecondHalfAsync()
+        {
+            var fileName = "Input.txt";
+
+            if (_useDemoFile)
+                fileName = "DemoInput.txt";
+
+            var fileContent = await File.ReadAllTextAsync(fileName);
+
+            var idRanges = fileContent.Split(',', StringSplitOptions.TrimEntries);
+
+            var invalidIds = new List<long>();
+
+            foreach (var idRange in idRanges)
+            {
+                var range = IdRange.Create(idRange);
+
+                var rangeIds = range.GetRangeIds();
+
+                invalidIds.AddRange(rangeIds.Where(x => !IsIdValidForSecondHalf(x)));
+            }
+
+            var sum = invalidIds.Sum();
+
+            Console.WriteLine($"The solution is {sum}. Hope you liked it. Press any key to close the console.");
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Returns whether the <paramref name="id"/> is valid, 
+        /// as per the specification of the first half if the exercise
+        /// </summary>
+        /// <param name="id">The id</param>
+        /// <returns></returns>
+        private static bool IsIdValidForFirstHalf(long id)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(id);
+
+            var stringRepresentation = id.ToString(CultureInfo.InvariantCulture);
+
+            if (stringRepresentation.Length % 2 != 0)
+                return true;
+
+            var halfLength = stringRepresentation.Length / 2;
+
+            var firstPart = stringRepresentation.AsSpan(0, halfLength);
+
+            var secondPart = stringRepresentation.AsSpan(halfLength);
+
+            var arePartsDifferent = !firstPart.Equals(secondPart, StringComparison.OrdinalIgnoreCase);
+
+            return arePartsDifferent;
+        }
+
+        /// <summary>
+        /// Returns whether the <paramref name="id"/> is valid, 
+        /// as per the specification of the second half if the exercise
+        /// </summary>
+        /// <param name="id">The id</param>
+        /// <returns></returns>
+        private static bool IsIdValidForSecondHalf(long id)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(id);
+
+            var stringRepresentation = id.ToString(CultureInfo.InvariantCulture);
+
+            var possibleSequences = GetPossibleSequences(stringRepresentation);
+
+            foreach (var possibleSequence in possibleSequences)
+            {
+                var chunks = stringRepresentation.Chunk(possibleSequence.Length)
+                                                    .Select(x => new string(x))
+                                                    .ToList();
+
+                if (chunks.Count > 1)
+                {
+                    var areAllChunksMatching = chunks.All(x => x.Equals(possibleSequence, StringComparison.OrdinalIgnoreCase));
+
+                    if(areAllChunksMatching)
+                        return false;                    
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns all the available sequences that can be extracted
+        /// from the starting character of the <paramref name="value"/>
+        /// until the half point of the sequence
+        /// </summary>
+        /// <param name="value">The value</param>
+        /// <returns></returns>
+        private static IEnumerable<string> GetPossibleSequences(string value)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value);
+
+            var halfLength = value.Length / 2;
+
+            for (int i = 1; i <= halfLength; i++)
+            {
+                yield return value[..i];
+            }
+        }
+    }
+}
